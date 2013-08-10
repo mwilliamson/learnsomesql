d5871e0db3c5647b66e841888e6c72bd207617bf
Michael Williamson <mike@zwobble.org>
2013-08-10 21:50:27 +0100

    Implement static rendering of lessons

--- learnsomesql/app.py
+++ learnsomesql/app.py
@@ -2,7 +2,7 @@ import flask
 import os
 
 
-def create_app():
+def create_app(lessons):
     app = flask.Flask(
         "learnsomesql",
         template_folder=_package_path("templates"),
@@ -12,31 +12,51 @@ def create_app():
     @app.route("/")
     def hello():
         return "Hello World!"
-
     
-    lessons = [
-        Lesson("simple-selects", "Simple SELECTs"),
-        Lesson("select-star", "SELECT *"),
-    ]
-    
-    def _find_lesson(slug):
-        for lesson in lessons:
+    def _find_lesson_index(slug):
+        for index, lesson in enumerate(lessons):
             if lesson.slug == slug:
-                return lesson
-        
+                return index
+                
+        return None
+    
     
     @app.route("/lesson/<lesson_slug>")
     def lesson(lesson_slug):
-        lesson = _find_lesson(lesson_slug)
-        return flask.render_template("lesson.html", lesson=lesson)
+        lesson_index = _find_lesson_index(lesson_slug)
+        if lesson_index is None:
+            return flask.abort(404)
+        else:
+            all_lessons = map(LessonViewModel, lessons)
+            lesson = all_lessons[lesson_index]
+            
+            if lesson_index + 1 < len(all_lessons):
+                next_lesson = all_lessons[lesson_index + 1]
+            else:
+                next_lesson = None
+            
+            return flask.render_template(
+                "lesson.html",
+                lesson=lesson,
+                next_lesson=next_lesson,
+                all_lessons=all_lessons,
+            )
         
     return app
     
 
 class Lesson(object):
-    def __init__(self, slug, name):
+    def __init__(self, slug, title, description):
         self.slug = slug
-        self.name = name
+        self.title = title
+        self.description = description
+        
+
+class LessonViewModel(object):
+    def __init__(self, lesson):
+        self.title = lesson.title
+        self.url = flask.url_for("lesson", lesson_slug=lesson.slug)
+        self.description = lesson.description
 
     
 def _package_path(path):
@@ -44,5 +64,9 @@ def _package_path(path):
 
 
 if __name__ == "__main__":
-    create_app().run(debug=True)
+    sample_lessons = [
+        Lesson("simple-selects", "Simple SELECTs", "<p>SELECTs are simple</p>"),
+        Lesson("select-star", "SELECT *", "<p>Don't use SELECT * in code</p>"),
+    ]
+    create_app(sample_lessons).run(debug=True)
 
---
+++ tests/app_tests.py
@@ -0,0 +1,42 @@
+from nose.tools import istest, assert_equal, assert_in, assert_not_in
+
+from learnsomesql.app import create_app, Lesson
+
+
+@istest
+def lesson_slug_is_used_to_find_lesson():
+    client = _create_client()
+    response = client.get("/lesson/select-star")
+    assert_equal(200, response.status_code)
+    assert_in("<h2>SELECT *</h2>", response.data)
+    
+    
+@istest
+def next_lesson_is_shown_if_not_last_lesson():
+    client = _create_client()
+    response = client.get("/lesson/select-star")
+    assert_in("<h2>Next lesson: WHERE clauses</h2>", response.data)
+    
+    
+@istest
+def next_lesson_is_not_shown_if_last_lesson():
+    client = _create_client()
+    response = client.get("/lesson/where-clauses")
+    assert_not_in("<h2>Next lesson", response.data)
+
+
+@istest
+def unknown_lesson_returns_404():
+    client = _create_client()
+    response = client.get("/lesson/bwah")
+
+
+def _create_client():
+    lessons = [
+        Lesson("simple-selects", "Simple SELECTs", "<p>SELECTs are simple</p>"),
+        Lesson("select-star", "SELECT *", "<p>Don't use SELECT * in code</p>"),
+        Lesson("where-clauses", "WHERE clauses", "<p>This is a WHERE clause</p>"),
+    ]
+    app = create_app(lessons)
+    return app.test_client()
+    
