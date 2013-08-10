12e2052fe46172337a87074a957d5388e4fa0c3f
Michael Williamson <mike@zwobble.org>
2013-08-10 20:46:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# learnsomesql
+
+## License
+
+[2-Clause BSD](http://opensource.org/licenses/BSD-2-Clause)
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='learnsomesql',
+    version='0.1.0',
+    description='Website for learnsomesql.com',
+    long_description=read("README"),
+    author='Michael Williamson',
+    url='http://github.com/mwilliamson/learnsomesql',
+    packages=['learnsomesql'],
+    install_requires=[
+        "sqlexecutor>=0.1.0,<0.2",
+    ],
+    license='BSD 2-Clause',
+)
