9c2bc1aa72950e0ca35fd271c48d8053773be179
Michael Williamson <mike@zwobble.org>
2013-08-11 17:18:46 +0100

    Split read_lessons into separate class

--- learnsomesql/courses.py
+++ learnsomesql/courses.py
@@ -4,7 +4,7 @@ from xml.dom import minidom
 from xmlfilter import filter_xml
 
 
-class CourseReader():
+class CourseReader(object):
     def __init__(self, executor):
         self._executor = executor
     
@@ -12,35 +12,42 @@ class CourseReader():
         document = filter_xml(minidom.parse(f), dialect)
         course_element = document.documentElement
         creation_sql = self._read_creation_sql(course_element)
-        lessons = self._read_lessons(course_element)
+        lessons_reader = LessonsReader(self._executor, creation_sql)
+        lessons = lessons_reader.read_lessons(course_element)
         return Course(creation_sql, lessons)
-        
+
     def _read_creation_sql(self, course_element):
-        creation_sql_element = self._find(course_element, "creation-sql")
+        creation_sql_element = _find(course_element, "creation-sql")
         if creation_sql_element is None:
             return []
         else:
             return [
                 command
-                for command in re.split(r"\s*\n\s*\n\s*", self._text(creation_sql_element).strip())
+                for command in re.split(r"\s*\n\s*\n\s*", _text(creation_sql_element).strip())
             ]
-            
-    def _read_lessons(self, course_element):
-        lessons_element = self._find(course_element, "lessons")
+
+
+class LessonsReader(object):
+    def __init__(self, executor, creation_sql):
+        self._executor = executor
+        self._creation_sql = creation_sql
+        
+    def read_lessons(self, course_element):
+        lessons_element = _find(course_element, "lessons")
         return map(
             self._read_lesson_element,
-            self._find_elements(lessons_element, "lesson"),
+            _find_elements(lessons_element, "lesson"),
         )
             
     def _read_lesson_element(self, lesson_element):
-        slug = self._text(self._find(lesson_element, "slug"))
-        title = self._text(self._find(lesson_element, "title"))
-        description = self._text(self._find(lesson_element, "description"))
+        slug = _text(_find(lesson_element, "slug"))
+        title = _text(_find(lesson_element, "title"))
+        description = _text(_find(lesson_element, "description"))
         
-        questions_element = self._find(lesson_element, "questions")
+        questions_element = _find(lesson_element, "questions")
         questions = map(
             self._read_question_element,
-            self._find_elements(questions_element, "question"),
+            _find_elements(questions_element, "question"),
         )
         
         return Lesson(
@@ -51,8 +58,8 @@ class CourseReader():
         )
         
     def _read_question_element(self, question_element):
-        description = self._inner_xml(self._find(question_element, "description"))
-        correct_query = self._text(self._find(question_element, "correct-query"))
+        description = _inner_xml(_find(question_element, "description"))
+        correct_query = _text(_find(question_element, "correct-query"))
         
         return Question(
             description=description,
@@ -61,33 +68,36 @@ class CourseReader():
         )
         
 
-    def _find(self, node, name):
-        for child in node.childNodes:
-            if child.nodeType == child.ELEMENT_NODE and child.tagName == name:
-                return child
-                
-    def _find_elements(self, node, name):
-        if node is None:
-            return []
-            
-        def _is_element(node):
-            return node.nodeType == node.ELEMENT_NODE and node.tagName == name
-        
-        return filter(_is_element, node.childNodes)
+def _find(node, name):
+    for child in node.childNodes:
+        if child.nodeType == child.ELEMENT_NODE and child.tagName == name:
+            return child
 
-    def _text(self, node):
-        if node is None:
-            return ""
-        elif node.nodeType == node.TEXT_NODE:
-            return node.nodeValue
-        else:
-            return "".join(map(self._text, node.childNodes))
-            
-    def _inner_xml(self, node):
-        if node is None:
-            return ""
-        else:
-            return "".join(child.toxml() for child in node.childNodes).strip()
+
+def _find_elements(node, name):
+    if node is None:
+        return []
+        
+    def _is_element(node):
+        return node.nodeType == node.ELEMENT_NODE and node.tagName == name
+    
+    return filter(_is_element, node.childNodes)
+
+
+def _text(node):
+    if node is None:
+        return ""
+    elif node.nodeType == node.TEXT_NODE:
+        return node.nodeValue
+    else:
+        return "".join(map(_text, node.childNodes))
+
+
+def _inner_xml(node):
+    if node is None:
+        return ""
+    else:
+        return "".join(child.toxml() for child in node.childNodes).strip()
 
 
 class Course(object):
